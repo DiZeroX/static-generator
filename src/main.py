f7f00e2472a72ea318584a056631ffca8e876275
DiZeroX <ericmun17@gmail.com>
2024-05-28 06:27:51 -0400

    added TextNode class

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+  sample_text_node = TextNode("Text node content", "italic", "https://www.google.com")
+  print(sample_text_node)
+  
+if __name__=="__main__":
+  main()
