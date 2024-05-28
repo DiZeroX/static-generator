25fa72ba151eeb2764db8f5d56dedd39b730cea5
DiZeroX <ericmun17@gmail.com>
2024-05-28 06:38:12 -0400

    added unit test for TextNode

---
+++ src/test_textnode.py
@@ -0,0 +1,40 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+  def test_eq(self):
+    node = TextNode("This is a text node", "bold")
+    node2 = TextNode("This is a text node", "bold")
+    self.assertEqual(node, node2)
+    
+  def test_eq_url(self):
+    node = TextNode("This is a text node", "bold", "https://google.com")
+    node2 = TextNode("This is a text node", "bold", "https://google.com")
+    self.assertEqual(node, node2)
+    
+  def test_eq_url_none(self):
+    node = TextNode("This is a text node", "bold", None)
+    node2 = TextNode("This is a text node", "bold", None)
+    self.assertEqual(node, node2)
+    
+  def test_not_eq(self):
+    node = TextNode("This is a text node", "italic")
+    node2 = TextNode("This is a text node", "bold")
+    self.assertNotEqual(node, node2)
+    
+  def test_not_eq_content(self):
+    node = TextNode("This is a different text node", "italics")
+    node2 = TextNode("This is a text node", "italics")
+    self.assertNotEqual(node, node2)
+    
+  def test_repr(self):
+    node = TextNode("This is a text node", "text", "https://www.boot.dev")
+    self.assertEqual(
+        "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+    )
+    
+
+if __name__ == "__main__":
+  unittest.main()
