8ff77ff239192d93f1f95ef25f63e4c213a19acc
DiZeroX <ericmun17@gmail.com>
2024-06-05 04:17:36 -0400

    calculate block type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,12 @@
+import re
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown):
   blocks = []
   temp_block = ""
@@ -12,4 +21,28 @@ def markdown_to_blocks(markdown):
   if temp_block:
     blocks.append(temp_block.strip())
   return blocks
+
+def block_to_block_type(markdown):
+  if re.findall(r"^#{1,6} ", markdown):
+    return block_type_heading
+  if re.findall(r"```.*```", markdown):
+    return block_type_code
+  lines = markdown.splitlines()
+  quote_counter = 0
+  unordered_list_counter = 0
+  ordered_list_counter = 0
+  for line in lines:
+    if re.findall(r"^>", line):
+      quote_counter += 1
+    elif re.findall(r"[*-] ", line):
+      unordered_list_counter += 1
+    elif re.findall(f"{ordered_list_counter + 1}. ", line):
+      ordered_list_counter += 1
+  if len(lines) == quote_counter:
+    return block_type_quote
+  if len(lines) == unordered_list_counter:
+    return block_type_unordered_list
+  if len(lines) == ordered_list_counter:
+    return block_type_ordered_list
+  return block_type_paragraph
   
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,7 +1,14 @@
 import unittest
 
 from block_markdown import (
-  markdown_to_blocks
+  markdown_to_blocks,
+  block_to_block_type,
+  block_type_paragraph,
+  block_type_heading,
+  block_type_code,
+  block_type_quote,
+  block_type_unordered_list,
+  block_type_ordered_list,
 )
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -15,6 +22,30 @@ class TestBlockMarkdown(unittest.TestCase):
     ]
     
     self.assertEqual(expected_result, markdown_to_blocks(input))
+  
+  def test_block_to_block_type_heading(self):
+    input = "### Heading Test"
+    self.assertEqual(block_type_heading, block_to_block_type(input))
+    
+  def test_block_to_block_type_code(self):
+    input = "```Code Test```"
+    self.assertEqual(block_type_code, block_to_block_type(input))
+    
+  def test_block_to_block_type_quote(self):
+    input = ">Test quote\n>quote2"
+    self.assertEqual(block_type_quote, block_to_block_type(input))
+    
+  def test_block_to_block_type_unordered_list(self):
+    input = "* item1\n- item2"
+    self.assertEqual(block_type_unordered_list, block_to_block_type(input))
+    
+  def test_block_to_block_type_ordered_list(self):
+    input = "1. item1\n2. item2\n3. item3"
+    self.assertEqual(block_type_ordered_list, block_to_block_type(input))
+    
+  def test_block_to_block_type_paragraph(self):
+    input = "paragraph test"
+    self.assertEqual(block_type_paragraph, block_to_block_type(input))
     
 if __name__ == "__main__":
   unittest.main()
